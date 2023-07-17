55458bb53672b574ec3e2b3bc23f7f0c0664f6f9
Lauren Bockelman <lauren_t_bockelman@progressive.com>
2023-07-17 16:39:35 -0400

    monday finish

---
+++ frontend/app/src/app/pages/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+ 
+
+@Component({
+    templateUrl: "./dashboard.component.html",
+    selector: 'app-dashboard',
+    standalone: true
+})
+export class DashboardComponent {
+
+ 
+
+}
