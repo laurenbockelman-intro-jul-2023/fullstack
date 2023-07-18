19baaa10020f6c20adf8a391d18125757ac3ad13
Lauren Bockelman <lauren_t_bockelman@progressive.com>
2023-07-18 15:25:50 -0400

    fixing to write entry tests

---
+++ frontend/app/src/app/features/todos/components/entry/entry.component.cy.ts
@@ -0,0 +1,7 @@
+import { EntryComponent } from './entry.component';
+
+describe(EntryComponent.name, () => {
+  beforeEach(() => cy.mount(EntryComponent));
+
+  it('Renders', () => {});
+});
--- frontend/app/src/app/features/todos/models/index.ts
+++ frontend/app/src/app/features/todos/models/index.ts
@@ -3,3 +3,5 @@ export type TodoListItemModel = {
   description: string;
   status: 'Later' | 'Now' | 'Waiting' | 'Completed';
 };
+
+export type TodoListEntryModel = Pick<TodoListItemModel, 'description'>;
